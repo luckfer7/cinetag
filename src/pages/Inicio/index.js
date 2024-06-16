dabd9dbc3a99fbadc54c63b8b0c92c09d3397ff0
luckfer7 <lucasferreira059@gmail.com>
2024-06-16 16:40:06 -0300

    só fazer o deploy

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,10 +3,19 @@ import Banner from "components/Banner";
 import Card from "components/Cards";
 
 import Titulo from "components/Titulo";
-import videos from 'json/db.json'
 import styles from './Inicio.module.css'
+import { useEffect, useState } from "react";
 
 function Inicio () {
+    const [videos, setVideos] = useState([]);
+
+    useEffect (() => {
+        fetch('https://my-json-server.typicode.com/luckfer7/cinetag-api/videos')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideos(dados)
+        })
+    }, [])
     return (
         <>
             <Banner imagem="Home"/>
